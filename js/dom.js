0c51b4685ae6e7c8b29c43a3573533101ad3cc15
Siri <xd42069xd69420@gmail.com>
2023-12-09 11:14:35 +0900

    removed useless old js

--- js/dom.js
+++
@@ -1,7 +0,0 @@
-import _p5 from './sketch.js';
-export * from './dom/make.js';
-export const ROOT_ELEMENT_ID = "root";
-export const MEDIA_CLASS = "media";
-export const SEEKER_CLASS = "seeker";
-export const br = () => _p5.createDiv("<br />");
-//# sourceMappingURL=dom.js.map
--- js/dom/make.d.ts
+++
@@ -1,3 +0,0 @@
-import * as dom from '../dom.js';
-export declare function makeVolume(): dom.Volume;
-export declare function makeMediaControls(): dom.MediaControls;
